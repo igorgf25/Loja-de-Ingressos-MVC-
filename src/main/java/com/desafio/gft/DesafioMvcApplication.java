6839dce6b03bce1700993b8eeff4791342d3b46a
iefn <igor.feliciano@gft.com>
2022-03-28 10:42:19 -0300

    Initial commit

---
+++ src/main/java/com/desafio/gft/DesafioMvcApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.gft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/gft/DesafioMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.gft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
