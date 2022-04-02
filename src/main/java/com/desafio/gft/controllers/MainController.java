6cd63bde915ab07ec52e8d80a26a3a43ac30a78f
iefn <igor.feliciano@gft.com>
2022-04-01 22:29:55 -0300

    Tela de login custumizada adicionada

--- src/main/java/com/desafio/gft/controllers/MainController.java
+++ src/main/java/com/desafio/gft/controllers/MainController.java
@@ -4,6 +4,7 @@ import com.desafio.gft.services.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,6 +17,12 @@ public class MainController {
     public ModelAndView index() {
         ModelAndView mv = new ModelAndView("index.html");
         mv.addObject("listaEventos", eventoService.listaEventos());
+        return mv;
+    }
+
+    @RequestMapping("login")
+    public ModelAndView login(@RequestParam(required = false) boolean erro) {
+        ModelAndView mv = new ModelAndView("usuario/login.html");
         return mv;
     }
 }
--- src/main/java/com/desafio/gft/security/WebSecurityConfig.java
+++ src/main/java/com/desafio/gft/security/WebSecurityConfig.java
@@ -2,14 +2,12 @@ package com.desafio.gft.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -24,9 +22,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/evento/**").hasRole("ADMIN")
                 .antMatchers("/casadeshow/**").hasRole("ADMIN")
                 .antMatchers("/usuario/formAdmin").hasRole("ADMIN")
+                .antMatchers("/usuario/form").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin().permitAll()
-                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+                .and().formLogin().loginPage("/login").permitAll().successForwardUrl("/");
     }
 
     @Override
@@ -37,6 +35,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception{
-        web.ignoring().antMatchers("../templates/**");
+        web.ignoring().antMatchers("/resources/**");
     }
+
+
 }
